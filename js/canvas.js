b2dc8302020760a8a4d0b4b3fcb2c2135b9e0ddf
situx <timo.homburg@gmx.de>
2019-09-21 20:58:45 +0200

    canvas update

--- js/canvas.js
+++ js/canvas.js
@@ -275,16 +275,45 @@ var blength=100
 var clength=120
 var dlength=120
 
- 
+
+function lineIntersect(x1,y1,x2,y2, x3,y3,x4,y4) {
+    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
+    if (isNaN(x)||isNaN(y)) {
+        return false;
+    } else {
+        if (x1>=x2) {
+            if (!(x2<=x&&x<=x1)) {return false;}
+        } else {
+            if (!(x1<=x&&x<=x2)) {return false;}
+        }
+        if (y1>=y2) {
+            if (!(y2<=y&&y<=y1)) {return false;}
+        } else {
+            if (!(y1<=y&&y<=y2)) {return false;}
+        }
+        if (x3>=x4) {
+            if (!(x4<=x&&x<=x3)) {return false;}
+        } else {
+            if (!(x3<=x&&x<=x4)) {return false;}
+        }
+        if (y3>=y4) {
+            if (!(y4<=y&&y<=y3)) {return false;}
+        } else {
+            if (!(y3<=y&&y<=y4)) {return false;}
+        }
+    }
+    return true;
+}
  
 function getPaleoCodeDirection(){
         var paleoCodeResult=""
         let sortBy = [{
             prop:'origx',
-            direction: 1
+            direction: -1
         },{
             prop:'origy',
-            direction: 1
+            direction: -1
         }];
         strokeArray=strokeArray.sort(function(a,b){
         let i = 0, result = 0;
@@ -329,13 +358,15 @@ function getPaleoCodeDirection(){
         if(stroke>0){
             previousStroke=strokeArray[stroke-1]
             currentStroke=strokeArray[stroke]
+            var linesintersect=lineIntersect(currentStroke["origx"],currentStroke["origy"],currentStroke["targetX"],currentStroke["targetY"],
+                                                     previousStroke["origx"],previousStroke["origy"],previousStroke["targetX"],previousStroke["targetY"])
+            console.log("Intersect? "+linesintersect)
             console.log(previousStroke["origx"]+"<"+currentStroke["origx"]+"="+(previousStroke["origx"]<currentStroke["origx"]))
             console.log(previousStroke["targetX"]+"<"+currentStroke["targetX"]+"="+(previousStroke["targetX"]<currentStroke["targetX"]))
             console.log("origx>targetX?"+currentStroke["origx"]>previousStroke["targetX"])
-            if(previousStroke["origx"]<currentStroke["origx"] && previousStroke["targetX"]<currentStroke["targetX"] && currentStroke["origx"]>previousStroke["targetX"]){
+            if(previousStroke["targetX"]<currentStroke["origx"]){
                 paleoCodeResult+="-"
-            }else if(previousStroke["origy"]<currentStroke["origy"] && previousStroke["targetY"]<currentStroke["targetY"] 
-                && currentStroke["origy"]>previousStroke["targetY"] && previousStroke["targetX"]<currentStroke["origx"]){
+            }else if(previousStroke["targetY"]<currentStroke["origy"]){
                 paleoCodeResult+=":"
             }
         }
